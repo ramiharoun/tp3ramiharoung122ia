ebf97de957d3570523c3b03580303d96a5f0c2f7
Rami Haroun <harounrami@outlook.com>
2025-11-09 22:05:50 +0100

    modification du guidetouristiquerresources

--- src/main/java/com/example/tp3ramiharoung122ia/resources/GuideTouristiqueResource.java
+++ src/main/java/com/example/tp3ramiharoung122ia/resources/GuideTouristiqueResource.java
@@ -1,16 +1,22 @@
 package com.example.tp3ramiharoung122ia.resources;
 
+import com.example.tp3ramiharoung122ia.llm.LlmClientRest;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/guide")
+@Produces(MediaType.APPLICATION_JSON)
 public class GuideTouristiqueResource {
+
+    @Inject
+    LlmClientRest llm;
 
     @GET
     @Path("lieu/{ville_ou_pays}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String[] villeOuPays(@PathParam("ville_ou_pays") String lieu) {
-        return new String[]{ lieu }; // Étape 2 : ["Paris"]
+    public Response villeOuPays(@PathParam("ville_ou_pays") String lieu) {
+        String resultat = llm.assistant().guide(lieu);
+        return Response.ok(resultat).build();
     }
-
 }
