6c43f06b5ccceef0b8892afe42aaf3bbca553191
Rami Haroun <harounrami@outlook.com>
2025-11-09 17:44:31 +0100

    version 1 du projet

---
+++ src/main/java/com/example/tp3ramiharoung122ia/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.tp3ramiharoung122ia;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
